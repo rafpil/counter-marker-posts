2d75b123301a282aa82be09dbb5399b0b004f3c2
Rafal <Rafal>
2022-01-28 23:26:01 +0100

    Added total card

--- src/context/dataCtx.js
+++ src/context/dataCtx.js
@@ -1,20 +1,32 @@
 import React, { useState } from "react";
+import { AddToTotal, RemoveFromTotal } from "../components/counter/counter";
 
 const DataContext = React.createContext({
   data: [],
+  total: [],
   add: () => {},
+  edit: () => {},
   remove: () => {},
 });
 
 export const DataContextProvider = (props) => {
   const [data, setData] = useState([]);
+  const [total, setTotal] = useState({name: 'Suma', km: [0,0,0,0,0,0,0,0,0,0], he: [0,0,0,0,0,0,0,0,0,0]});
   
   const add = (itemToAdd) => {
     setData(data.concat([itemToAdd]));
+    setTotal(AddToTotal(itemToAdd, total));
   }
 
-  const remove = (itemToRemove) => {
-      
+  const edit = (idItemToEdit, nameToEdit) => {
+    let indexToEdit = data.findIndex(item => item.id === idItemToEdit);
+    data[indexToEdit].name = nameToEdit; 
+  }
+
+  const remove = (idItemToRemove) => {
+    let newdata = data.filter(item => item.id !== idItemToRemove);
+    setData(newdata);
+    setTotal(RemoveFromTotal(data[data.findIndex(item => item.id === idItemToRemove)], total));
   }
   
 
@@ -22,7 +34,9 @@ export const DataContextProvider = (props) => {
     <DataContext.Provider
       value={{
         data: data,
+        total: total,
         add: add,
+        edit: edit,
         remove: remove,
       }}
     >
--- src/pageElements/dataCard.js
+++ src/pageElements/dataCard.js
@@ -4,18 +4,21 @@ import Card from "../components/card/card";
 import NoDataInfo from "../components/noDataInfo/noDataInfo";
 
 const DataCard = () => {
-
   const dataCtx = useContext(DataContext);
-
+  
   return (
     <>
-      <div className="flex flex-row justify-center gap-3">
+      <div className="flex flex-row justify-center py-4">
         <div className="">{!dataCtx.data.length && <NoDataInfo />}</div>
       </div>
-      <div className="flex flex-row justify-center gap-3">
-        {dataCtx.data.map((item, index) => (
-          <Card key={`card ${index}`} item={item} />
-        ))}
+      <div className="flex flex-row flex-wrap justify-center">
+        {dataCtx.data.length !== 0 && (
+          <Card key={`total`} item={dataCtx.total} alloweEdit={false} />
+        )}
+        {dataCtx.data.length !== 0 &&
+          dataCtx.data.map((item, index) => (
+            <Card key={`card-${index}`} item={item} />
+          ))}
       </div>
     </>
   );
