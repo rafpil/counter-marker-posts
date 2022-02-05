be4af17cb5e7ce2705b2b966c478167d81a8a6d8
Rafal <Rafal>
2022-02-05 23:04:07 +0100

    Code refactoring - card.js

--- src/components/card/card.js
+++ src/components/card/card.js
@@ -4,6 +4,8 @@ import { FaEdit, FaSave } from "react-icons/fa";
 import { RiDeleteBin5Fill } from "react-icons/ri";
 import DataContext from "../../context/dataCtx";
 
+import "./card.css";
+
 const Card = ({ item, alloweEdit = true }) => {
   const [editInput, setEditInput] = useState(false);
   const [valueInput, setValueInput] = useState(item.name);
@@ -19,57 +21,54 @@ const Card = ({ item, alloweEdit = true }) => {
   };
 
   return (
-      <div className="flex justify-center w-[33.333333%]">
-        <div className="block rounded-lg shadow shadow-[#062147] bg-white border-2 border-[#142F56] max-w-sm text-center w-full m-2">
-          <div className="relative py-3 px-6 font-bold text-[#142F56] border-b border-gray-400 flex justify-center items-center">
-            {!editInput && (
-              <div className="pt-[4px] pb-[5px] truncate w-[12rem]">
-                {" "}
-                {valueInput}{" "}
-              </div>
-            )}
-            {editInput && (
-              <input
-                className="text-center py-[4px] border-b w-[12rem] border-[#142F56] focus-visible:outline-none"
-                value={valueInput}
-                onChange={changeHandler}
-              />
-            )}
-            {alloweEdit && (
-              <div className="absolute flex right-[5px] cursor-pointer">
-                {!editInput && (
-                  <FaEdit
-                    className="mx-1 hover:shadow-2xl hover:shadow-[#062147] hover:text-[#062147]"
-                    onClick={() => setEditInput(!editInput)}
-                  />
-                )}
-                {editInput && (
-                  <FaSave
-                    className="mx-1 hover:shadow-2xl hover:shadow-[#062147] hover:text-[#062147]"
-                    onClick={() => {
-                      setEditInput(!editInput);
-                      dataCtx.edit(item.id, valueInput);
-                    }}
-                  />
-                )}
-                {!editInput && (
-                  <RiDeleteBin5Fill
-                    className="mx-1 hover:shadow-2xl hover:shadow-[#062147] hover:text-[#062147]"
-                    onClick={() => dataCtx.remove(item.id)}
-                  />
-                )}
-              </div>
-            )}
-          </div>
-          <div className="p-1">
-            <h5 className="text-[#142F56] font-medium mb-2">
-              {alloweEdit && (`Kilometrarz: ${item.start} - ${item.finish}`)}
-              {!alloweEdit && ('Łączna ilość dla wszystkich danych')}
-            </h5>
-            <Table item={item} />
-          </div>
+    <div className="card-data">
+      <div className="card-body">
+        <div className="card-header">
+          {!editInput && (
+            <div className="card-header--main-text"> {valueInput} </div>
+          )}
+          {editInput && (
+            <input
+              className="card-header--input"
+              value={valueInput}
+              onChange={changeHandler}
+            />
+          )}
+          {alloweEdit && (
+            <div className="card-box-icon">
+              {!editInput && (
+                <FaEdit
+                  className="card-icon"
+                  onClick={() => setEditInput(!editInput)}
+                />
+              )}
+              {editInput && (
+                <FaSave
+                  className="card-icon"
+                  onClick={() => {
+                    setEditInput(!editInput);
+                    dataCtx.edit(item.id, valueInput);
+                  }}
+                />
+              )}
+              {!editInput && (
+                <RiDeleteBin5Fill
+                  className="card-icon card-icon--delete"
+                  onClick={() => dataCtx.remove(item.id)}
+                />
+              )}
+            </div>
+          )}
+        </div>
+        <div className="p-1">
+          <h5 className="card-header--sub-text">
+            {alloweEdit && `Kilometrarz: ${item.start} - ${item.finish}`}
+            {!alloweEdit && "Łączna ilość dla wszystkich danych"}
+          </h5>
+          <Table item={item} />
         </div>
       </div>
+    </div>
   );
 };
 
