f9c02a527bef08d825d372df9a437402d6fd3807
Rafal <Rafal>
2022-02-05 16:35:47 +0100

    Added own colors to tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,18 @@
 module.exports = {
-  content: ["./src/**/*.{js,jsx,ts,tsx}",],
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        app_blue: {
+          10: "#142F56",
+          20: "#062147",
+        },
+        app_orange: {
+          10: "#FBBE00",
+          20: "#b48802",
+        },
+      },
+    },
   },
   plugins: [],
 };
