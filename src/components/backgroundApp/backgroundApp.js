fac19fb315dcd752662920e4dbc319990c7b8aa6
Rafal <Rafal>
2022-02-05 16:47:03 +0100

    Code refactoring - backgroundApp.js

--- src/components/backgroundApp/backgroundApp.js
+++ src/components/backgroundApp/backgroundApp.js
@@ -1,11 +1,8 @@
 import React from "react";
+import "./backgroundApp.css";
 
 const BackgroundApp = (props) => {
-  return (
-    <div className="bg-[#FBBE00] border-2 border-[#b48802] rounded w-10/12 bg-opacity-60">
-      {props.children}
-    </div>
-  );
+  return <div className="background-app">{props.children}</div>;
 };
 
 export default BackgroundApp;
