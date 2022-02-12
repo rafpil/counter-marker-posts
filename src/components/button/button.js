56bb5d198f6e135d1f80471009b8694a0e098bce
Rafal <Rafal>
2022-02-12 20:34:56 +0100

    Added disabled feature

--- src/components/button/button.js
+++ src/components/button/button.js
@@ -1,13 +1,14 @@
-import React, { useContext } from "react";
-import DataContext from "../../context/dataCtx";
+import React from "react";
 
-const Button = ({ click }) => {
-  const dataCtx = useContext(DataContext);
+
+const Button = ({ click, disabled }) => {
+
   return (
     <button
       type="button"
-      className="inline-block px-7 py-3 bg-[#142F56] text-white font-bold text-sm leading-snug uppercase rounded shadow-md hover:bg-[#062147] hover:shadow-lg focus:bg-[#062147] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#062147] active:shadow-lg transition duration-150 ease-in-out w-full"
-      onClick={() => dataCtx.add(click())}
+      disabled={!disabled}
+      className="inline-block px-7 py-3 bg-[#142F56] text-white font-bold text-sm leading-snug uppercase rounded shadow-md hover:bg-[#062147] hover:shadow-lg focus:bg-[#062147] focus:shadow-lg focus:outline-none focus:ring-0 active:bg-[#062147] active:shadow-lg transition duration-150 ease-in-out w-full disabled:bg-[#142f56e1] disabled:cursor-not-allowed"
+      onClick={click}
     >
       Przelicz i dodaj  
     </button>
