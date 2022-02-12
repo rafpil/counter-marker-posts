a311cd4f58abb0611624eb2e7f7fae18648e8f4e
Rafal <Rafal>
2022-02-12 22:33:03 +0100

    Moved styles to separate file

--- src/PageElements/HelpSection/HelpIcon.js
+++ src/PageElements/HelpSection/HelpIcon.js
@@ -1,10 +1,12 @@
 import React from "react";
 import { FaInfo } from "react-icons/fa";
 
+import "./HelpIcon.css";
+
 const HelpIcon = () => {
   return (
-    <div className="w-full flex justify-end p-2">
-        <FaInfo className="rounded-full text-[2rem] bg-[#142F56] text-white p-[.3rem] shadow-md hover:bg-[#062147] hover:shadow-lg focus:bg-[#062147] focus:shadow-lg" />
+    <div className="help-icon">
+      <FaInfo className="help-icon--info" />
     </div>
   );
 };
